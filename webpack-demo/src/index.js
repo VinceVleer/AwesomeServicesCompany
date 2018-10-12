4b94d4a6a2305524f7de72e37657c57eea8a5654
Vince W <vwelbergen@competa.com>
2018-10-12 13:04:27 +0200

    added webpack

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+$(function() {
+
+	$('.subtitle__item, .subtitle__item--2, .subtitle__item--3, .subtitle__item--4').hover(function(){
+		$(this).css("background-color", "lightgrey");
+		}, function(){
+		$(this).css("background-color", "white");
+	});
+
+	$('.form__button').hover(function(){
+		$(this).css("background-color", "#444");
+		}, function(){
+		$(this).css("background-color", "black");
+	});
+
+});
