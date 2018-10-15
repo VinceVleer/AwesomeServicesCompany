4c2743af94007308dc42716f097611f1c41c5fe5
Vince W <vwelbergen@competa.com>
2018-10-15 11:52:54 +0200

    Applied feedback

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,6 @@
-import _ from 'lodash';
-
 $(function() {
 
-	$('.subtitle__item, .subtitle__item--2, .subtitle__item--3, .subtitle__item--4').hover(function(){
+	$('.subtitle__item').hover(function(){
 		$(this).css("background-color", "lightgrey");
 		}, function(){
 		$(this).css("background-color", "white");
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+	return gulp.src('./app/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./app/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('./scss/**/*.scss', ['sass']);
 });
