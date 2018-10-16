e62a0ee879867ddfbffe1a10fe8c554938b83d00
Vince W <vwelbergen@competa.com>
2018-10-16 14:18:04 +0200

    Improved folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 gulp.task('sass', function () {
-	return gulp.src('./app/scss/**/*.scss')
+	return gulp.src('./src/assets/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./app/css'));
+		.pipe(gulp.dest('./dist/assets/css'));
 });
  
 gulp.task('sass:watch', function () {
-	gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('src/assets/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('pipe', function () {
+	return gulp.src(['src/assets/img/*'])
+	.pipe(gulp.dest('dist/assets/img'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: './app/src/index.js',
+	entry: './src/assets/js/index.js',
 	output: {
 		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+		path: path.resolve(__dirname, 'dist/assets/js')
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{from:'src/assets/img',to:'../img/'},
+			{from:'src/assets/js/jquery-3.3.1.min.js',to:'../js/'},
+			{from:'src/index.html',to:'../../'},
+		]), 
+	],
 };
